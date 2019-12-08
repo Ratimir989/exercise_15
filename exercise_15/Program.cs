abee027a3bab2feb1ceacd2c3ae664673026a296
Ratimir <ra0953029364@gmail.com>
2019-12-08 10:48:20 +0200

    Добавьте файлы проекта.

---
+++ exercise_15/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercise_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 64; i >= 0; i -= 4)
+            {
+                Console.WriteLine($"кроликов: {i / 4}  гусей: {(64 - i) / 2}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
